c7ab7aa1dff505a2eb1f333a7308d5a964422c08
Smile <xcxerxes@aliyun.com>
2018-12-10 08:28:57 +0800

    init

---
+++ twitter-Objective/Navigator/NavigationTabBar.h
@@ -0,0 +1,17 @@
+//
+//  NavigationTabBar.h
+//  twitter-Objective
+//
+//  Created by xiacan on 2018/12/8.
+//  Copyright © 2018 iotek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NavigationTabBar : UITabBar
+
+@end
+
+NS_ASSUME_NONNULL_END
